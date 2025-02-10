fb1836840d5b2179e58993e2c56246b8f22907e6
mohamoha0000 <moham3iof@gmail.com>
2025-02-10 08:40:23 +0000

    x o

--- xo game terminale/main.cpp
+++ xo game terminale/main.cpp
@@ -4,17 +4,33 @@ void update(string box[]);
 
 string box[9]={"1","2","3",
                "4","5","6",
-               "7","8","9"};
+              "7","8","9"};
+
+string player="*";
+int choice;
 int main(){
     update(box);
+    while (true)
+    {
+     cout<<"player " <<player<<":";
+     cin>>choice;
+     if (choice<9 && choice>0 && box[choice]!="*" && box[choice]!="#"){
+        box[choice]=player;
+        update(box);
+        if (player=="*"){
+            player="#";
+        }else{
+            player="*";
+        }
+     }
+    }
+    
 }
 void update(string box[]){
-    int a;
     for(int x=0;x<3;x++){
         for(int i=0;i<3;i++){
             cout<<"|"<<box[(i+x*3)];
         }
         cout<<"|"<<endl;
     }
-    cin>>a;
 }
