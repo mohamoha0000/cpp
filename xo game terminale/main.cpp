6de00233be9217c1da74bfac32f9a6a42f7b0e0f
mohamoha0000 <moham3iof@gmail.com>
2025-02-11 12:41:53 +0000

    hhh

--- xo game terminale/main.cpp
+++ xo game terminale/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 void update(string box[]);
-
+string chek(string box[]);
 string box[9]={"1","2","3",
                "4","5","6",
                "7","8","9"};
@@ -33,5 +33,18 @@ void update(string box[]){
             cout<<"|"<<box[(i+x*3)];
         }
         cout<<"|"<<endl;
+    }
+}
+
+string  chek(string box[]){
+    for(int x =0;x<3;x++){
+        for(int i=0;i<3;i++){
+            if(box[3*x]==box[1+3*x] && box[1+3*x]==box[2+3*x] && (box[3*x] =="*" || box[3*x]=="#")){
+
+            }
+            if(box[x]==box[x] && box[3*x]==box[6+3*x] && (box[3*x] =="*" || box[3*x]=="#")){
+
+            }
+        }
     }
 }
